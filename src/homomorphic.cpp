bb82f402821d369b9551e5955f6f1a170177da28
Carlos Eduardo Ramalho <cadubentzen@gmail.com>
2017-10-23 09:44:58 -0300

    homomorphic

--- src/homomorphic.cpp
+++ src/homomorphic.cpp
@@ -7,21 +7,44 @@
 using namespace cv;
 using namespace std;
 
-int gamma_l = 255;
-int gamma_l_slider = 255;
-int gamma_l_slider_max = 255;
+float gl = 0;
+int gl_slider = 0;
+int gl_slider_max = 100;
 
-int gamma_h = 255;
-int gamma_h_slider = 255;
-int gamma_h_slider_max = 255;
+float gh = 0;
+int gh_slider = 50;
+int gh_slider_max = 100;
 
-int hue_gain = 0;
-int hue_gain_slider = 0;
-int hue_gain_slider_max = 255;
+float d0 = 0;
+int d0_slider = 50;
+int d0_slider_max = 100;
+
+float c = 0;
+int c_slider = 5;
+int c_slider_max = 100;
+
+char TrackbarName[50];
+
+Mat imaginaryInput, complexImage, multsp;
+Mat padded, filter, mag;
+Mat_<float> realInput, zeros;
+vector<Mat> planos;
+
+// valor do ruido
+float mean;
+
+// guarda tecla capturada
+char key;
+Mat image, imagegray, tmp; 
+// valores ideais dos tamanhos da imagem
+// para calculo da DFT
+int dft_M, dft_N;
+
+char *filename;
 
 // troca os quadrantes da imagem da DFT
 void deslocaDFT(Mat& image ){
-    Mat tmp, A, B, C, D;
+    Mat tmp2, A, B, C, D;
 
     // se a imagem tiver tamanho impar, recorta a regiao para
     // evitar cópias de tamanho desigual
@@ -38,61 +61,100 @@ void deslocaDFT(Mat& image ){
     D = image(Rect(cx, cy, cx, cy));
 
     // A <-> D
-    A.copyTo(tmp);  D.copyTo(A);  tmp.copyTo(D);
+    A.copyTo(tmp2);  D.copyTo(A);  tmp2.copyTo(D);
 
     // C <-> B
-    C.copyTo(tmp);  B.copyTo(C);  tmp.copyTo(B);
+    C.copyTo(tmp2);  B.copyTo(C);  tmp2.copyTo(B);
 }
 
-void on_trackbar_gamma_l(int, void*) {
+void on_trackbar_homomorphic(int, void*) {
+    gl = (float) gl_slider / 100.0;
+    gh = (float) gh_slider / 100.0;
+    d0 = 25.0 * d0_slider / 100.0;
+    c  = (float) c_slider  / 100.0;
+
+    cout << "gl = " << gl << endl;
+    cout << "gh = " << gh << endl;
+    cout << "d0 = " << d0 << endl;
+    cout << "c = "  << c  << endl;
+
+    image = imread(filename);
+    cvtColor(image, imagegray, CV_BGR2GRAY);
+    imshow("original", imagegray);
+
+    // realiza o padding da imagem
+    copyMakeBorder(imagegray, padded, 0,
+                   dft_M - image.rows, 0,
+                   dft_N - image.cols,
+                   BORDER_CONSTANT, Scalar::all(0));
+
+    // limpa o array de matrizes que vao compor a
+    // imagem complexa
+    planos.clear();
+    // cria a compoente real
+    realInput = Mat_<float>(padded);
+    // insere as duas componentes no array de matrizes
+    planos.push_back(realInput);
+    planos.push_back(zeros);
+
+    // combina o array de matrizes em uma unica
+    // componente complexa
+    merge(planos, complexImage);
+
+    // calcula o dft
+    dft(complexImage, complexImage);
+
+    // realiza a troca de quadrantes
+    deslocaDFT(complexImage);
+    
+    // filtro homomorfico
+	for(int i=0; i < tmp.rows; i++){
+		for(int j=0; j < tmp.cols; j++){
+			float d2 = (i-dft_M/2)*(i-dft_M/2)+(j-dft_N/2)*(j-dft_N/2);
+            //cout << "d2 = " << d2 << endl;
+			tmp.at<float> (i,j) = (gh-gl)*(1.0 - (float)exp(-(c*d2/(d0*d0)))) + gl;
+		}
+	}
+
+    // cria a matriz com as componentes do filtro e junta
+    // ambas em uma matriz multicanal complexa
+    Mat comps[]= {tmp, tmp};
+    merge(comps, 2, filter);
+    
+    // aplica o filtro frequencial
+    mulSpectrums(complexImage,filter,complexImage,0);
+
+    // troca novamente os quadrantes
+    deslocaDFT(complexImage);
+
+    // calcula a DFT inversa
+    idft(complexImage, complexImage);
+
+    // limpa o array de planos
+    planos.clear();
+
+    // separa as partes real e imaginaria da
+    // imagem filtrada
+    split(complexImage, planos);
+
+    // normaliza a parte real para exibicao
+    normalize(planos[0], planos[0], 0, 1, CV_MINMAX);
+    imshow("filtrada", planos[0]);
 
 }
 
-void on_trackbar_gamma_h(int, void*) {
+int main(int argc, char** argv){
+    
+    namedWindow("original", WINDOW_NORMAL);
+    namedWindow("filtrada", WINDOW_NORMAL);
 
-}
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <img_path>" << endl;
+        return 1;
+    }
 
-void on_trackbar_d_zero(int, void*) {
-
-}
-
-void on_trackbar_c(int, void*) {
-
-}
-
-int main(int , char**){
-    VideoCapture cap;   
-    Mat imaginaryInput, complexImage, multsp;
-    Mat padded, filter, mag;
-    Mat image, imagegray, tmp; 
-    Mat_<float> realInput, zeros;
-    vector<Mat> planos;
-
-    // valor do ruido
-    float mean;
-
-    // guarda tecla capturada
-    char key;
-
-    // valores ideais dos tamanhos da imagem
-    // para calculo da DFT
-    int dft_M, dft_N;
-
-    // Inicializar trackbars
-	/*sprintf( TrackbarName, "gamma_l" );
-	createTrackbar( TrackbarName, "func_image",
-					&start_focus_slider,
-					start_focus_slider_max,
-					on_trackbar_start_focus );
-    */
-    // abre a câmera default
-    cap.open(0);
-    if(!cap.isOpened())
-        return -1;
-
-    // captura uma imagem para recuperar as
-    // informacoes de gravação
-    cap >> image;
+    filename = argv[1];
+    image = imread(filename);
 
     // identifica os tamanhos otimos para
     // calculo do FFT
@@ -119,73 +181,38 @@ int main(int , char**){
     // e imaginaria do filtro ideal
     tmp = Mat(dft_M, dft_N, CV_32F);
 
-    // prepara o filtro passa-baixas ideal
-    for(int i=0; i<dft_M; i++){
-        for(int j=0; j<dft_N; j++){
-            if((i-dft_M/2)*(i-dft_M/2)+(j-dft_N/2)*(j-dft_N/2) < RADIUS*RADIUS){
-                tmp.at<float> (i,j) = 1.0;
-            }
-        }
-    }
+    
+    // Inicializar trackbars
+	sprintf( TrackbarName, "gamma_l" );
+	createTrackbar( TrackbarName, "filtrada",
+					&gl_slider,
+					gl_slider_max,
+					on_trackbar_homomorphic );
+    
+	sprintf( TrackbarName, "gamma_h" );
+	createTrackbar( TrackbarName, "filtrada",
+					&gh_slider,
+					gh_slider_max,
+					on_trackbar_homomorphic );
+	
+    sprintf( TrackbarName, "d_zero" );
+	createTrackbar( TrackbarName, "filtrada",
+					&d0_slider,
+					d0_slider_max,
+					on_trackbar_homomorphic );
+	
+    sprintf( TrackbarName, "c" );
+	createTrackbar( TrackbarName, "filtrada",
+					&c_slider,
+					c_slider_max,
+					on_trackbar_homomorphic );
 
-    // cria a matriz com as componentes do filtro e junta
-    // ambas em uma matriz multicanal complexa
-    Mat comps[]= {tmp, tmp};
-    merge(comps, 2, filter);
+    on_trackbar_homomorphic(100, NULL);
 
-    for(;;){
-        cap >> image;
-        cvtColor(image, imagegray, CV_BGR2GRAY);
-        imshow("original", imagegray);
-
-        // realiza o padding da imagem
-        copyMakeBorder(imagegray, padded, 0,
-                       dft_M - image.rows, 0,
-                       dft_N - image.cols,
-                       BORDER_CONSTANT, Scalar::all(0));
-
-        // limpa o array de matrizes que vao compor a
-        // imagem complexa
-        planos.clear();
-        // cria a compoente real
-        realInput = Mat_<float>(padded);
-        // insere as duas componentes no array de matrizes
-        planos.push_back(realInput);
-        planos.push_back(zeros);
-
-        // combina o array de matrizes em uma unica
-        // componente complexa
-        merge(planos, complexImage);
-
-        // calcula o dft
-        dft(complexImage, complexImage);
-
-        // realiza a troca de quadrantes
-        deslocaDFT(complexImage);
-
-        // aplica o filtro frequencial
-        mulSpectrums(complexImage,filter,complexImage,0);
-
-        // troca novamente os quadrantes
-        deslocaDFT(complexImage);
-
-        cout << complexImage.size().height << endl;
-        // calcula a DFT inversa
-        idft(complexImage, complexImage);
-
-        // limpa o array de planos
-        planos.clear();
-
-        // separa as partes real e imaginaria da
-        // imagem filtrada
-        split(complexImage, planos);
-
-        // normaliza a parte real para exibicao
-        normalize(planos[0], planos[0], 0, 1, CV_MINMAX);
-        imshow("filtrada", planos[0]);
-
+    while (1) {
         key = (char) waitKey(10);
         if( key == 27 ) break; // esc pressed!
     }
+
     return 0;
 }
